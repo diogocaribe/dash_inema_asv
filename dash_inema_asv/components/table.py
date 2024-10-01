d751dc9df5528980413f56b4061e42612fb5e50a
Diogo Caribé <dogosousa@gmail.com>
2024-10-01 14:40:13 -0300

    Refatorando o front-end da tabela

--- dash_inema_asv/components/table.py
+++ dash_inema_asv/components/table.py
@@ -1,11 +1,12 @@
 import json
-from dash import html, dash_table, dcc, html, Input, Output, callback
+from dash import html, dash_table, html, Input, Output, callback
 import geopandas as gpd
 
 
 table_div = html.Div(
     [
-        html.P("Processos Autorização de Supressão Vegetação", className="titulo-box"),
+        html.P("Processos Autorização de Supressão Vegetação", 
+               className="titulo-box", style={'padding': '0% 0% 2% 0%'}),
         html.Div(id="tabela-processo"),
     ],
     className="div-box-table",
@@ -59,26 +60,21 @@ def update_tabela(dados):
                 },  # Estinho da linha entre as celulas
                 style_table={
                     "border": "1px solid #DFDFDF",
-                    "height": "483px",
                     "overflowY": "auto",
                     "border-radius": "4px 4px 0px 0px",
+                    "height": '28vh',
                 },  # Borda ao redor da tabela
                 style_header={
-                    # 'display': 'flex',
-                    # 'justify-content': 'space-between',
                     "backgroundColor": "#F4F4F6",
-                    "height": "37px",
-                    "whiteSpace": "normal",
-                    "position": "sticky",  # Fixa o cabeçalho
-                    "top": "0",  # Define a posição do cabeçalho no topo
                 },
                 style_cell={
                     "textAlign": "center",
                     "font-family": "Roboto",
-                    "font-size": "12px",
+                    "font-size": "0.8rem",
                     "font-weight": 400,
                     "line-height": "13.5px",
                     "letter-spacing": "0.005em",
                 },
+                fixed_rows={'headers': True},
         )
     return tabela
